ca22207e3808274e8b11ae0d1262e5df2db0bea8
Gre-mie <114353201+Gre-mie@users.noreply.github.com>
2026-01-13 20:48:03 +0000

    removed unused moduals and tweeked things a bit

--- conf.lua
+++ conf.lua
@@ -1,8 +1,7 @@
 function love.conf(t)
-	print("love.conf started...") -- TEST:
-
 	-- program general
 	t.version = "11.5"
+	t.window.icon = "resources/images/icon.png" -- INFO: may not show on small window bars
 	t.gammacorrect = true
 
 	-- window
@@ -11,5 +10,22 @@ function love.conf(t)
 	t.window.width = 1280
 	t.window.height = 800
 
-	print("love.conf ended")
+	-- saving
+	-- WARNING: I dont need these right now, but will when creating .exe .app
+
+	-- t.appendidentity = true 	--for testing
+	-- t.appendidentity = false 	-- default, will be used to save files outside of application eg game save directory (AppData on windows)
+	t.identity = "painthack" -- INFO: the name of the directory the exicutible will be saved as/in
+
+	-- input
+	t.accelerometerjoystick = false -- WARNING: this should be true for applications exe type
+
+	-- moduals
+	-- INFO: unused moduals should be false
+	t.modules.joystick = false
+	t.modules.physics = false
+	t.modules.video = false
+	t.modules.data = false -- WARNING: may need this later for saving
+	t.modules.audio = false -- WARNING: may need this later for playing sounds
+	t.modules.audio = false -- WARNING: may need this later for decoding sound files
 end
