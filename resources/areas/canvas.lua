8d408506fe87f517a4b3e0af2d18540ad1098c8f
Gre-mie <114353201+Gre-mie@users.noreply.github.com>
2026-01-16 09:53:21 +0000

    add canvas

--- resources/areas/canvas.lua
+++ resources/areas/canvas.lua
@@ -2,15 +2,36 @@ local canvas = {}
 
 -- INFO: the drawing area of the window
 
-function canvas:New()
-	local obj = {x= , y= , height= , width= }
+function canvas:New(x, y, width, height)
+	local obj = { name = "canvas", x = x, y = y, height = height, width = width }
 
 	self.__index = self
 	return setmetatable(obj, self)
 end
 
-function canvas:Draw() 
+function canvas:Debug()
+	print("canvas")
+	print("x: " .. self.x .. " y: " .. self.y)
+	print("width: " .. self.width .. " height: " .. self.height)
+	print()
+end
 
+function canvas:Draw()
+	-- TEST: vv
+	linewidth = 10
+	if frame <= 1 then
+		print("draw canvas...")
+	end
+	love.graphics.setColor(0.3, 0.4, 0.1)
+	love.graphics.setLineWidth(linewidth, "smooth")
+	love.graphics.rectangle(
+		"line",
+		self.x + linewidth / 2,
+		self.y + linewidth / 2,
+		self.width - linewidth,
+		self.height - linewidth
+	)
+	-- TEST: ^^
 end
 
 return canvas
--- ui.lua
+++ ui.lua
@@ -1,19 +1,26 @@
+local uicanvas = require("resources/areas/canvas")
+
 local ui = {}
 
 -- INFO: ui holds all the areas and updates the elements
 --
 function ui:New()
+	--window.width, window.height
+
 	-- TODO: create section objects with their buttons, set their draw positions
 
 	-- TEST: vv
 	local padding = 10
+
+	local canvas = uicanvas:New(200, 200, window.width - 200, window.height - 200) -- WARNING: draw area should be made last, and fill remaining space
+
 	-- TODO: create base area interface with New, Draw, Debug methods
 	-- program bar interface, inherit add buttons (overwrite functions)
 	local programBar =
 		{ name = "program bar", x = 0, y = 0, width = window.width, height = 100, padding = padding, buttons = {} } -- area:NEW()
 	-- TEST: ^^
 
-	local obj = { areas = { programBar } }
+	local obj = { areas = { programBar, canvas } }
 
 	self.__index = self
 	return setmetatable(obj, self)
@@ -39,6 +46,10 @@ function ui:Draw()
 	-- draw section
 	for i = 1, #self.areas do
 		local section = self.areas[i]
+		if section.name == "canvas" then
+			section:Draw()
+			goto testloop
+		end
 		love.graphics.setLineWidth(linewidth, "smooth")
 		love.graphics.setColor(0.5, 0.1, 0.4)
 		love.graphics.rectangle(
@@ -65,6 +76,8 @@ function ui:Draw()
 				button.height
 			)
 		end
+
+		::testloop::
 	end
 
 	-- TEST: ^^
