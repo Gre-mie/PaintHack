8175e19671990b7feb40cfa410abb42cde10c265
Gre-mie <114353201+Gre-mie@users.noreply.github.com>
2026-01-25 20:04:36 +0000

    add colour file, incomplete

---
+++ resources/colours.lua
@@ -0,0 +1,14 @@
+-- INFO: holds all colours.
+	-- colours to be set when drawing to the window: rgb 0-255 converted to rgb 0-1
+	-- colours for terminal print are strings that use excape codes
+
+local colours = {
+	
+
+	
+	Warning = "\27[33mWARNING: \27[0m",
+	Error = "\27[31mERROR: \27[0m",
+
+}
+
+return colours
